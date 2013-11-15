ffedda1993c3cc4108c741bd55b4a11d35be78a2
Adèle Delamarche <adele.delamarche@gmail.com>
2013-11-15 18:46:15 +0100

    Add south introspection for SeparatedValuesField

--- courriers/fields.py
+++ courriers/fields.py
@@ -61,3 +61,6 @@ class SeparatedValuesField(models.CharField):
 
     def formfield(self, **kwargs):
         return MultipleChoiceField(choices=self.choices)
+
+from south.modelsinspector import add_introspection_rules
+add_introspection_rules([], ["^courriers\.fields\.SeparatedValuesField"])
