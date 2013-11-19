d9b3f20eba5a18cffc9ae2022a22c06be5880bf5
Adèle Delamarche <adele.delamarche@gmail.com>
2013-11-19 15:01:51 +0100

    Update admin.py

--- courriers/admin.py
+++ courriers/admin.py
@@ -50,7 +50,7 @@ class NewsletterListAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'languages', 'created_at',)
 
 
-admin.site.register(Newsletter, Newsletter)
+admin.site.register(Newsletter, NewsletterAdmin)
 admin.site.register(NewsletterItem, NewsletterItemAdmin)
 admin.site.register(NewsletterSubscriber, NewsletterSubscriberAdmin)
 admin.site.register(NewsletterList, NewsletterListAdmin)
