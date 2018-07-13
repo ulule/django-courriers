4e7a098106adc0d9ae4d501613e12a7f05f9b184
Florent Messa <florent.messa@gmail.com>
2018-07-13 15:35:32 +0200

    fix: imports

--- courriers/views.py
+++ courriers/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import ListView, DetailView, FormView, TemplateView
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.utils.functional import cached_property
